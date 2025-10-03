c27a738fa5c5969aeb82c2ddf3dd0f1ecd6070b1
amersyafiq <amersyafiq1103@gmail.com>
2025-10-03 10:39:49 +0800

    Disable all fetch request

--- frontend/src/pages/Course/Course.jsx
+++ frontend/src/pages/Course/Course.jsx
@@ -18,14 +18,14 @@ export function Course({ currSession, isLoadingSession, schedCourses, setSchedCo
 
     useEffect(() => {
         const fetchFilterData = async () => {
-            const url =  "https://simsweb4.uitm.edu.my/estudent/class_timetable/cfc/select.cfc?method=FAC_lII1II11I1lIIII11IIl1I111I&key=All&page=1&page_limit=30";
-            const res = await axios.get(url, {
-            headers: {
-                "User-Agent": "Mozilla/5.0",
-                "Referer": "https://simsweb4.uitm.edu.my/estudent/class_timetable/index.htm"
-            }})
+            // const url =  "https://simsweb4.uitm.edu.my/estudent/class_timetable/cfc/select.cfc?method=FAC_lII1II11I1lIIII11IIl1I111I&key=All&page=1&page_limit=30";
+            // const res = await axios.get(url, {
+            // headers: {
+            //     "User-Agent": "Mozilla/5.0",
+            //     "Referer": "https://simsweb4.uitm.edu.my/estudent/class_timetable/index.htm"
+            // }})
 
-            console.log(res.data)
+            // console.log(res.data)
             
             // try {
             //     const [rawCam, rawFac] = await Promise.all([
